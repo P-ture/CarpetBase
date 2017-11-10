fa13f98ac8554ac0384ff6aa34c6aa31aa51bf0d
Wildhoney <adam.timberlake@gmail.com>
2017-11-10 18:46:47 +0000

    Setup token verification

--- src/server/api/auth.js
+++ src/server/api/auth.js
@@ -4,22 +4,61 @@ import setCookie from 'set-cookie';
 import connect from '../database';
 
 /**
+ * @method fetchUser
+ * @param {Object} request
+ * @param {Object} response
+ * @return {Promise}
+ */
+export async function fetchUser(request, response) {
+
+    try {
+        
+        const db = connect();
+
+        // Verify the JSON web token from the user's cookies.
+        const record = jwt.verify(request.cookies.jwttoken, process.env.CARPETBASE_SECRET);
+        const [{ id, username }] = await db.select().from('users').where('username', record.username);
+
+        response.send({ id, username, authenticated: true });
+    
+    } catch (err) {
+
+        // Otherwise the user is unauthenticated.
+        response.send({ authenticated: false });
+
+    }
+
+} 
+
+/**
  * @method authenticate
  * @param {Object} request
  * @param {Object} response
  * @return {Promise} 
  */
-export default async function authenticate(request, response) {
+export async function authenticate(request, response) {
 
     const db = connect();
-    const [{ username, password, salt }] = await db.select().from('users').where('username', request.query.username);
-    const isValid = await argon2.verify(password, `${salt}${request.query.password}`);
-    const token = jwt.sign({ username }, process.env.CARPETBASE_SECRET);
+    
+    try {
 
-    isValid && setCookie('jwttoken', token, {
-        res: response
-    });
+        // Gather the password and associated hashing salt based on the supplied username.
+        const [{ username, password, salt }] = await db.select().from('users').where('username', request.params.username);
+    
+        // Verify their Argon2 hashed password with the salt, and sign the JWT.
+        const isValid = await argon2.verify(password, `${salt}${request.params.password}`);
+        const token = jwt.sign({ username }, process.env.CARPETBASE_SECRET);
+    
+        // Save the JWT to cookies if we have been successfully authenticated.
+        isValid && setCookie('jwttoken', token, { res: response });
+    
+        return response.send({ authenticated: isValid, token: isValid ? token : null });
 
-    return response.send({ authorised: isValid, token: isValid ? token : null });
+    } catch (err) {
+
+        // Otherwise the supplied username doesn't have a record in the database.
+        return response.send({ authenticated: false, token: null });
+
+    }
     
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,21 @@
 import http from 'http';
 import express from 'express';
 import compression from 'compression';
+import cookieParser from 'cookie-parser';
 import { PORT } from '../js/config';
 import renderApplication from './universal';
-import authenticate from './api/auth';
+import { login, fetchUser, authenticate } from './api/auth';
 import fetchPage from './api/page';
 
 const app = express();
 app.use(compression());
+app.use(cookieParser());
 
 const server = http.createServer(app);
 
 app.get(/[/|.*\.html]$/, renderApplication);
 app.get('/api/authenticate.json', authenticate);
+app.get('/api/user.json', fetchUser);
 app.get('/api/page/:slug.json', fetchPage);
 
 app.use(express.static('public/assets'));
