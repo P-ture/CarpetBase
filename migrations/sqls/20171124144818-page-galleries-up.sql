01fbdd32cd5762d4b3d9ed1b0b7ccadae6c9fdfc
Wildhoney <adam.timberlake@gmail.com>
2017-11-29 09:26:59 +0000

    Changed Heroku app name

--- migrations/sqls/20171124144818-page-galleries-up.sql
+++ migrations/sqls/20171124144818-page-galleries-up.sql
@@ -2,5 +2,6 @@ CREATE TABLE `page_galleries` (
   `page_id` int(11) unsigned NOT NULL,
   `gallery_id` int(11) NOT NULL,
   `order` int(11) NOT NULL DEFAULT '0',
+  `page_link_id` int(11) DEFAULT NULL,
   UNIQUE KEY `composite_id` (`gallery_id`,`page_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
